a8b034d5cdafcd36e490c6da89ca14edfb7f6ea5
Brett Zamir <brettz9@yahoo.com>
2019-07-07 19:12:00 +0800

    INCOMPLETE: exclude .git and this JSON file from the static server! - Begin local strategy - add `addUser` script for adding user to file database - npm: Update devDeps ash-nazg/eslint-plugin-jsdoc and axe-core

--- server/access.js
+++ server/access.js
@@ -1,5 +1,26 @@
 const app = require('multipronged')();
 const passport = require('@passport-next/passport');
+const LocalStrategy = require('@passport-next/passport-local').Strategy;
+
+const User = {
+  async findOne ({username}) {
+    return {
+      verifyPassword (password) {
+        // eslint-disable-next-line global-require
+        require('maptext-admin.json');
+      }
+    };
+  }
+};
+
+passport.use(new LocalStrategy(
+  async function (username, password) {
+    const user = await User.findOne({username});
+    return user && user.verifyPassword(password)
+      ? user
+      : false;
+  }
+));
 
 app.use([
   passport.initialize(),
