3bd53bd196fce3ff13e367b674820ea3eba862f0
Brett Zamir <brettz9@yahoo.com>
2020-04-02 18:19:19 +0800

    - Linting: Lint eslintrc file

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,52 +1,60 @@
+'use strict';
 module.exports = {
-    "env": {
-      "browser": true,
-      "es6": true
-    },
-    "settings": {
-      "polyfills": [
-        "Array.isArray",
-        "console",
-        "DataTransfer",
-        "DOMParser",
-        "Error",
-        "fetch",
-        "JSON",
-        "location.href",
-        "Map",
-        "navigator.clipboard",
-        "Notification",
-        "Notification.permission",
-        "Notification.requestPermission",
-        "Number.isNaN",
-        "Number.parseFloat",
-        "Number.parseInt",
-        "Object.assign",
-        "Object.entries",
-        "Object.keys",
-        "Promise"
-      ]
-    },
-    "overrides": [
-      {
-        "files": ["server/**"],
-        "rules": {
-          "strict": "off",
-          "import/unambiguous": "off",
-          "import/no-commonjs": "off"
-        }
+  env: {
+    browser: true,
+    es6: true
+  },
+  settings: {
+    polyfills: [
+      'Array.isArray',
+      'console',
+      'DataTransfer',
+      'DOMParser',
+      'Error',
+      'fetch',
+      'JSON',
+      'location.href',
+      'Map',
+      'navigator.clipboard',
+      'Notification',
+      'Notification.permission',
+      'Notification.requestPermission',
+      'Number.isNaN',
+      'Number.parseFloat',
+      'Number.parseInt',
+      'Object.assign',
+      'Object.entries',
+      'Object.keys',
+      'Promise'
+    ]
+  },
+  overrides: [
+    {
+      files: '.eslintrc.js',
+      extends: ['plugin:node/recommended-script'],
+      rules: {
+        'import/no-commonjs': 0
       }
-    ],
-    "extends": ["ash-nazg/sauron", "plugin:testcafe/recommended"],
-    "globals": {
-      "Atomics": "readonly",
-      "SharedArrayBuffer": "readonly"
     },
-    "parser": "babel-eslint",
-    "parserOptions": {
-      "ecmaVersion": 2018,
-      "sourceType": "module"
-    },
-    "rules": {
+    {
+      files: ['server/**'],
+      rules: {
+        strict: 'off',
+        'import/unambiguous': 'off',
+        'import/no-commonjs': 'off'
+      }
     }
+  ],
+  extends: ['ash-nazg/sauron', 'plugin:testcafe/recommended'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  rules: {
+  }
 };
