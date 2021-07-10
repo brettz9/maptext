816789cdffe88d4c213e47adb8db719b79b4ca75
Brett Zamir <brettz9@yahoo.com>
2021-07-10 13:10:38 +0800

    - More nogin integration (WIP)

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@
 
 const path = require('path');
 const jsonServer = require('json-server');
-const accessMiddleware = require('./access.js');
 const textSearchMiddleware = require('./text-search.js');
 
 // Todo: Need to redirect if not logged in?
@@ -13,6 +12,6 @@ module.exports = (app) => {
   const middlewares = jsonServer.defaults({
     static: path.join(process.cwd(), '.')
   });
-  app.use(jsonPath, [accessMiddleware, textSearchMiddleware, ...middlewares]);
+  app.use(jsonPath, [textSearchMiddleware, ...middlewares]);
   app.use(router);
 };
