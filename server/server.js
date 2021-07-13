866db336ff89f6108cdafa4d0f760ac2e81c463c
Brett Zamir <brettz9@yahoo.com>
2021-07-13 16:42:23 +0800

    - npm: Update nogin - npm: Update devDeps.

--- server/server.js
+++ server/server.js
@@ -6,12 +6,11 @@ const textSearchMiddleware = require('./text-search.js');
 
 // Todo: Need to redirect if not logged in?
 module.exports = (app) => {
-  const jsonPath = 'maptext.json';
-  const router = jsonServer.router(jsonPath);
+  const router = jsonServer.router('maptext.json');
   router.db._.id = 'name';
   const middlewares = jsonServer.defaults({
     static: path.join(process.cwd(), '.')
   });
-  app.use(jsonPath, [textSearchMiddleware, ...middlewares]);
+  app.use('/maps', [textSearchMiddleware, ...middlewares]);
   app.use(router);
 };
